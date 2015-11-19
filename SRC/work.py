40f26c408357b9d8960cc7cf1fa7749ab05aaf9a
LiuBei <liubeitongxue@icloud.com>
2015-11-19 10:32:40 +0800

    clear code

--- SRC/work.py
+++ SRC/work.py
@@ -2,40 +2,36 @@ import urllib.request
 import urllib.parse
 import json
 import codecs
+from datetime import date
 
 def main():
     #获取json文件
     with urllib.request.urlopen('http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US') as response:
         result = response.read()
-        #print(result)
         s = result.decode()
         s = codecs.encode(s,encoding='utf-8')
         s = s.decode()
         dict = json.loads(s)
-        #print(list)
-        #print(type(dict))
         keys = dict.keys()
-        #print(keys)
-        #print(type(keys))
-        # print(dict)
-        #print(codecs.encode(dict['images'],encoding='utf-8'))
         list = dict['images']
-        #print(len(list))
+        #获取url
         url = list[0]['url']
         print(url)
 
         #urllib.request.urlretrieve(url,'E:/Workspace/20151117BingWallpaperWorkspace/BingWallpaper/SRC')
-        with urllib.request.urlopen(url) as web:
-            jpg = web.read()
-            DstDir = "E:\\Workspace\\20151117BingWallpaperWorkspace\\BingWallpaper\\SRC\\"
-            FileName = 'BingWallpaper.jpg'
-            File = open(DstDir+FileName,'wb')
+        with urllib.request.urlopen(url) as dataValue:
+            jpg = dataValue.read()
+            DstDir = "D:\\WorkSpace\\20151111GitWorkspace\\BingWallpaper\\SRC\\"
+            FileName1 = 'BingWallpaper'
+            FileName2 = date.today().isoformat()
+            FileNameEnd = '.jpg'
+            FileName=FileName1+FileName2+FileNameEnd
+
+            #下载图片并保存在当前路径
+            File = open(DstDir + FileName,'wb')
             File.write(jpg)
             File.close()
-            print('end')
-
-#获取url
-#下载图片并保存在当前路径
+            print('【请从',DstDir,'查看下载结果】')
 
 if __name__ == '__main__':
     main()
